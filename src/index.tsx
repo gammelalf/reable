efb9bcf7b9ed697d3474e37c73c6d15f1f69fd76
gammelalf <gammelalf@hopfen.space>
2023-01-21 15:35:36 +0100

    Improved showcase code

--- src/index.tsx
+++ src/index.tsx
@@ -2,38 +2,65 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import Table, { Cell, Column, Head } from "./table";
+import data from "./data.json";
 
 type MainState = {};
 
 class Main extends React.Component<{}, MainState> {
     render() {
-        const data = [
-            { id: 1, name: "Alice", age: 18, gender: "Female" },
-            { id: 2, name: "Bob", age: 19, gender: "Male" },
-            { id: 3, name: "Charles", age: 32, gender: "Male" },
-            { id: 3, name: "Danielle", age: 69, gender: "Female" },
-            { id: -1, name: "Malory", age: NaN, gender: "Other" },
-        ];
         return (
             <Table>
-                <Column key="name">
+                <Column key="name" sortable>
                     <Head>Name</Head>
                     {data.map(({ id, name }) => (
-                        <Cell key={id}>{name}</Cell>
-                    ))}
-                </Column>
-                <Column key="age" sortable>
-                    <Head>Age</Head>
-                    {data.map(({ id, age }) => (
-                        <Cell key={id} sortKey={age}>
-                            {age.toString()}
+                        <Cell key={id} sortKey={id}>
+                            {name}
                         </Cell>
                     ))}
                 </Column>
-                <Column key="gender">
-                    <Head>Gender</Head>
-                    {data.map(({ id, gender }) => (
-                        <Cell key={id}>{gender}</Cell>
+                <Column
+                    key="fruit"
+                    sortable
+                    sortFn={(a: string, b: string) => a.localeCompare(b)}
+                >
+                    <Head>Fruit</Head>
+                    {data.map(({ id, fruit }) => (
+                        <Cell key={id} sortKey={fruit || ""}>
+                            {fruit || ""}
+                        </Cell>
+                    ))}
+                </Column>
+                <Column key="gender" sortable>
+                    <Head>Done</Head>
+                    {data.map(({ id, done }) => (
+                        <Cell key={id} sortKey={done ? 1 : 0}>
+                            <input type="checkbox" disabled checked={done} />
+                        </Cell>
+                    ))}
+                </Column>
+                <Column key="time" sortable>
+                    <Head>Date</Head>
+                    {data.map(({ id, timestamp }) => (
+                        <Cell key={id} sortKey={timestamp}>
+                            {new Date(timestamp * 1000).toLocaleDateString("en-US", {
+                                year: "numeric",
+                                month: "2-digit",
+                                day: "2-digit",
+                            })}
+                        </Cell>
+                    ))}
+                </Column>
+                <Column key="color" sortable>
+                    <Head>Color</Head>
+                    {data.map(({ id, color }) => (
+                        <Cell
+                            key={id}
+                            sortKey={color === "red" ? 0 : color === "green" ? 1 : 2}
+                        >
+                            <div style={{ backgroundColor: color, color: "transparent" }}>
+                                .
+                            </div>
+                        </Cell>
                     ))}
                 </Column>
             </Table>
